f0e449607a200331c98846cbcb891a781ad29b87
EduMogro <edumogro@outlook.com>
2021-04-16 18:46:53 -0300

    Blockchain is working

--- main.js
+++ main.js
@@ -9,20 +9,31 @@ class Block
         this.data = data;
         this.previusHash = previusHash;
         this.hash = this.createHash();
+        this.nonce = 0;
     }
 
     createHash()
     {
-        return SHA256(this.index + this.date + this.data).toString();
+        return SHA256(this.index + this.date + this.data + this.previusHash + this.nonce).toString();
+    }
+
+    mine(difficulty)
+    {
+        while (!this.hash.startsWith(difficulty))
+        {
+            this.nonce++;
+            this.hash = this.createHash();
+        }
     }
 
 }
 
 class BlockChain
 {
-    constructor (genesis)
+    constructor (genesis, difficulty = '00')
     {
         this.chain = [this.createFirstBlock(genesis)];
+        this.difficulty = difficulty;
     }
 
     createFirstBlock(genesis)
@@ -39,15 +50,35 @@ class BlockChain
     {
         let prevBlock = this.getLastBlock();
         let block = new Block(prevBlock.index + 1, data, prevBlock.hash);
+        block.mine(this.difficulty);
+        console.log('Minado! Hash: ' + block.hash + ' | Nonce: ' + block.nonce);
         this.chain.push(block);
+    }
+
+    isValid()
+    {
+        for (let i = 1; i < this.chain.length; i++)
+        {
+            let prevBlock = this.chain[i - 1];
+            let currBlock = this.chain[i];
+
+            if (currBlock.previusHash != prevBlock.hash)
+                return false;
+            if (currBlock.createHash() != currBlock.hash)
+                return false;
+            return true;
+        }
     }
 }
 
 // block = new Block(0,'test');
 // console.log(JSON.stringify(block,null,2));
 
-let eduCoin = new BlockChain('eduCoin');
+let eduCoin = new BlockChain('eduCoin', '000');
 eduCoin.addBlock('test1');
 eduCoin.addBlock('test2');
 
-console.log(JSON.stringify(eduCoin.chain,null,2));
+eduCoin.chain[1].data = 'test1';
+
+console.log(eduCoin.isValid());
+// console.log(JSON.stringify(eduCoin.chain,null,2));
