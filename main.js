569afdb1a854e23894aa84cd05f0d792e35dd90c
EduMogro <edumogro@outlook.com>
2021-04-16 10:44:08 -0300

    Block and BlockChain created

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
 const SHA256 = require('crypto-js/sha256');
 
-class Block {
+class Block
+{
     constructor (index, data, previusHash = '')
     {
         this.index = index;
         this.date = new Date();
         this.data = data;
         this.previusHash = previusHash;
-        this.hash = '';
+        this.hash = this.createHash();
     }
 
     createHash()
     {
-        
+        return SHA256(this.index + this.date + this.data).toString();
     }
 
 }
+
+class BlockChain
+{
+    constructor (genesis)
+    {
+        this.chain = [this.createFirstBlock(genesis)];
+    }
+
+    createFirstBlock(genesis)
+    {
+        return new Block(0,genesis);
+    }
+
+    getLastBlock()
+    {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(data)
+    {
+        let prevBlock = this.getLastBlock();
+        let block = new Block(prevBlock.index + 1, data, prevBlock.hash);
+        this.chain.push(block);
+    }
+}
+
+// block = new Block(0,'test');
+// console.log(JSON.stringify(block,null,2));
+
+let eduCoin = new BlockChain('eduCoin');
+eduCoin.addBlock('test1');
+eduCoin.addBlock('test2');
+
+console.log(JSON.stringify(eduCoin.chain,null,2));
